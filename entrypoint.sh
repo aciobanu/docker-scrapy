796dff58f61206d654d71079174099c1d51cff4d
Adi <aciobanu@users.noreply.github.com>
2016-03-01 08:22:12 +0200

    Evaluate the conditions for determining scrapy commands in a more natural order

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # handle scrapy commands
-scrapy_commands="$(scrapy | awk '/Available commands/,/^$/ { if ($1 != "Available" && NF) { print $1 }}')"
+scrapy_commands="$(scrapy | awk '/Available commands/,/^$/ { if (NF && $1 != "Available") { print $1 }}')"
 if (echo $scrapy_commands | grep -qw "$1"); then
     set -- scrapy $@
 fi
